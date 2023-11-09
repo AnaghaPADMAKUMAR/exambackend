ac9e08756a3d2a2f79c6bba64e0f33eac7f4a125
unknown <anaghapadmakumar@gmail.com>
2023-11-09 12:55:35 +0530

    next

---
+++ index.js
@@ -0,0 +1,17 @@
+const jsonserver=require('json-server')
+
+const server=jsonserver.create()
+
+const middleware=jsonserver.defaults()
+
+const router=jsonserver.router("db.json")
+
+const PORT=process.env.PORT || 4000
+
+server.use(middleware)
+
+server.use(router)
+
+server.listen(PORT,()=>{
+   console.log(`JSON Server satrted at Port Number ${PORT}`);
+})
